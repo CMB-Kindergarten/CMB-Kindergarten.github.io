c9e8763a9a0d5b7686ef0904e39d38997efa3956
wzr1998 <253864593@qq.com>
2018-12-26 16:53:03 +0800

    index almost over

--- js/index.js
+++ js/index.js
@@ -11,8 +11,7 @@ function window_onLoad(){
 
 
 
-
-/*function moveTopImg(){
+function moveTopImg(){
     let oPrompt = document.getElementById('prompt');
     let oPicWrap = oPrompt.getElementsByClassName('img')[0];
     let aImg = oPicWrap.getElementsByTagName('img');
@@ -22,19 +21,20 @@ function window_onLoad(){
     mouseOverClearTimer(oPicWrap, timer, nextPic, 2500);
 
     oPicWrap.now = 0;               //记录显示区域中图片下标
+
     function nextPic(){                         //自动轮播
         for(let i=0, len=aNum1.length; i<len; i++){
             aNum1[i].className = '';
         }
         if(oPicWrap.now === aImg.length-1){
             aNum1[0].className = 'active';
-            doMove(aImg[0], 'left', 10, 0);
+            cssDoMove(aImg[0], 'left', 0.5, 0);
         }else{
             aNum1[oPicWrap.now+1].className = 'active';
-            doMove(aImg[oPicWrap.now+1], 'left', 10, 0);
+            cssDoMove(aImg[oPicWrap.now+1], 'left', 0.5, 0);
         }
-        doMove(aImg[oPicWrap.now], 'left', 10, -270, function(){
-            aImg[oPicWrap.now].style.left = 270 + 'px';
+        cssDoMove(aImg[oPicWrap.now], 'left', 0.5, -270, function(){
+            aImg[oPicWrap.now].style.left = '270px';
             oPicWrap.now++;
             oPicWrap.now %= aImg.length;
         })
@@ -48,68 +48,6 @@ function window_onLoad(){
             aNum1[i].className = 'active';
             if(i<oPicWrap.now){
                 aImg[i].style.left = -270 + 'px';
-                doMove(aImg[i], 'left', 10, 0);
-                doMove(aImg[oPicWrap.now], 'left', 10, 270, function(){
-                    oPicWrap.now = i;
-                })
-            }else if(i>oPicWrap.now){
-                doMove(aImg[oPicWrap.now], 'left', 10, -270);
-                doMove(aImg[i], 'left', 10, 0, function(){
-                    aImg[oPicWrap.now].style.left = 270 + 'px';
-                    oPicWrap.now = i;
-                })
-            }
-        }
-    }
-}*/
-
-
-
-
-
-
-function moveTopImg(){
-    let oPrompt = document.getElementById('prompt');
-    let oPicWrap = oPrompt.getElementsByClassName('img')[0];
-    let aImg = oPicWrap.getElementsByTagName('img');
-    let aNum1 = oPrompt.getElementsByClassName('num')[0].getElementsByTagName('li');
-
-    let timer = setInterval(nextPic, 2500);
-    mouseOverClearTimer(oPicWrap, timer, nextPic, 2500);
-
-    oPicWrap.now = 0;               //记录显示区域中图片下标
-
-    nextPic();
-
-    function nextPic(){                         //自动轮播
-        for(let i=0, len=aNum1.length; i<len; i++){
-            aNum1[i].className = '';
-        }
-        if(oPicWrap.now === aImg.length-1){
-            aNum1[0].className = 'active';
-            cssDoMove(aImg[0], 'left', 0.5, 0);
-        }else{
-            aNum1[oPicWrap.now+1].className = 'active';
-            cssDoMove(aImg[oPicWrap.now+1], 'left', 0.5, 0);
-        }
-
-        cssDoMove(aImg[oPicWrap.now], 'left', 0.5, -270, function(){
-            cssDoMove(aImg[oPicWrap.now], 'left', 0, 270);
-            oPicWrap.now++;
-            oPicWrap.now %= aImg.length;
-        })
-
-
-    }
-
-    for(let i=0, len=aNum1.length; i<len; i++){
-        aNum1[i].onclick = function(){          //点击列表切换图片
-            for(let i=0, len=aNum1.length; i<len; i++){
-                aNum1[i].className = '';
-            }
-            aNum1[i].className = 'active';
-            /*if(i<oPicWrap.now){
-                aImg[i].style.left = -270 + 'px';
                 cssDoMove(aImg[i], 'left', 0.5, 0);
                 cssDoMove(aImg[oPicWrap.now], 'left', 0.5, 270, function(){
                     oPicWrap.now = i;
@@ -117,23 +55,13 @@ function moveTopImg(){
             }else if(i>oPicWrap.now){
                 cssDoMove(aImg[oPicWrap.now], 'left', 0.5, -270);
                 cssDoMove(aImg[i], 'left', 0.5, 0, function(){
-                    cssDoMove(aImg[oPicWrap.now], 'left', 0, -270);
+                    aImg[oPicWrap.now].style.left = 270 + 'px';
                     oPicWrap.now = i;
                 })
-            }*/
+            }
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
 
 
 function moveTopText(){
@@ -155,7 +83,6 @@ function moveTopText(){
 }
 
 
-
 function moveHappyTimeImg(){
     let picWrap = document.querySelector('#happyTime .pic');
     let aImg = document.querySelectorAll('#happyTime img');
@@ -175,25 +102,25 @@ function moveHappyTimeImg(){
         if(!picWrap.nextBtn) return;
         picWrap.nextBtn = !picWrap.nextBtn;
         if(picWrap.now === aImg.length-3){
-            doMove(aImg[picWrap.now], 'left', 10, -270);
-            doMove(aImg[picWrap.now+1], 'left', 10, 0);
-            doMove(aImg[picWrap.now+2], 'left', 10, 270);
-            doMove(aImg[0], 'left', 10, 540, endFn);
+            cssDoMove(aImg[picWrap.now], 'left', 0.5, -270);
+            cssDoMove(aImg[picWrap.now+1], 'left', 0.5, 0);
+            cssDoMove(aImg[picWrap.now+2], 'left', 0.5, 270);
+            cssDoMove(aImg[0], 'left', 0.5, 540, endFn);
         }else if(picWrap.now === aImg.length-2){
-            doMove(aImg[picWrap.now], 'left', 10, -270);
-            doMove(aImg[picWrap.now+1], 'left', 10, 0);
-            doMove(aImg[0], 'left', 10, 270);
-            doMove(aImg[1], 'left', 10, 540, endFn);
+            cssDoMove(aImg[picWrap.now], 'left', 0.5, -270);
+            cssDoMove(aImg[picWrap.now+1], 'left', 0.5, 0);
+            cssDoMove(aImg[0], 'left', 0.5, 270);
+            cssDoMove(aImg[1], 'left', 0.5, 540, endFn);
         }else if(picWrap.now === aImg.length-1){
-            doMove(aImg[picWrap.now], 'left', 10, -270);
-            doMove(aImg[0], 'left', 10, 0);
-            doMove(aImg[1], 'left', 10, 270);
-            doMove(aImg[2], 'left', 10, 540, endFn);
+            cssDoMove(aImg[picWrap.now], 'left', 0.5, -270);
+            cssDoMove(aImg[0], 'left', 0.5, 0);
+            cssDoMove(aImg[1], 'left', 0.5, 270);
+            cssDoMove(aImg[2], 'left', 0.5, 540, endFn);
         }else{
-            doMove(aImg[picWrap.now], 'left', 10, -270);
-            doMove(aImg[picWrap.now+1], 'left', 10, 0);
-            doMove(aImg[picWrap.now+2], 'left', 10, 270);
-            doMove(aImg[picWrap.now+3], 'left', 10, 540, endFn);
+            cssDoMove(aImg[picWrap.now], 'left', 0.5, -270);
+            cssDoMove(aImg[picWrap.now+1], 'left', 0.5, 0);
+            cssDoMove(aImg[picWrap.now+2], 'left', 0.5, 270);
+            cssDoMove(aImg[picWrap.now+3], 'left', 0.5, 540, endFn);
         }
         function endFn(){
             aImg[picWrap.now].style.left = 810 + 'px';
@@ -207,28 +134,28 @@ function moveHappyTimeImg(){
         picWrap.prevBtn = !picWrap.prevBtn;
         if(picWrap.now === 0){
             aImg[aImg.length-1].style.left = -270 + 'px';
-            doMove(aImg[aImg.length-1], 'left', 10, 0);
-            doMove(aImg[picWrap.now], 'left', 10, 270);
-            doMove(aImg[picWrap.now+1], 'left', 10, 540);
-            doMove(aImg[picWrap.now+2], 'left', 10, 810, endFn);
+            cssDoMove(aImg[aImg.length-1], 'left', 0.5, 0);
+            cssDoMove(aImg[picWrap.now], 'left', 0.5, 270);
+            cssDoMove(aImg[picWrap.now+1], 'left', 0.5, 540);
+            cssDoMove(aImg[picWrap.now+2], 'left', 0.5, 810, endFn);
         }else if(picWrap.now === aImg.length-1){
             aImg[picWrap.now-1].style.left = -270 + 'px';
-            doMove(aImg[picWrap.now-1], 'left', 10, 0);
-            doMove(aImg[picWrap.now], 'left', 10, 270);
-            doMove(aImg[0], 'left', 10, 540);
-            doMove(aImg[1], 'left', 10, 810, endFn);
+            cssDoMove(aImg[picWrap.now-1], 'left', 0.5, 0);
+            cssDoMove(aImg[picWrap.now], 'left', 0.5, 270);
+            cssDoMove(aImg[0], 'left', 0.5, 540);
+            cssDoMove(aImg[1], 'left', 0.5, 810, endFn);
         }else if(picWrap.now === aImg.length-2){
             aImg[picWrap.now-1].style.left = -270 + 'px';
-            doMove(aImg[picWrap.now-1], 'left', 10, 0);
-            doMove(aImg[picWrap.now], 'left', 10, 270);
-            doMove(aImg[picWrap.now+1], 'left', 10, 540);
-            doMove(aImg[0], 'left', 10, 810, endFn);
+            cssDoMove(aImg[picWrap.now-1], 'left', 0.5, 0);
+            cssDoMove(aImg[picWrap.now], 'left', 0.5, 270);
+            cssDoMove(aImg[picWrap.now+1], 'left', 0.5, 540);
+            cssDoMove(aImg[0], 'left', 0.5, 810, endFn);
         }else{
             aImg[picWrap.now-1].style.left = -270 + 'px';
-            doMove(aImg[picWrap.now-1], 'left', 10, 0);
-            doMove(aImg[picWrap.now], 'left', 10, 270);
-            doMove(aImg[picWrap.now+1], 'left', 10, 540);
-            doMove(aImg[picWrap.now+2], 'left', 10, 810, endFn);
+            cssDoMove(aImg[picWrap.now-1], 'left', 0.5, 0);
+            cssDoMove(aImg[picWrap.now], 'left', 0.5, 270);
+            cssDoMove(aImg[picWrap.now+1], 'left', 0.5, 540);
+            cssDoMove(aImg[picWrap.now+2], 'left', 0.5, 810, endFn);
         }
         function endFn(){
             picWrap.now--;
@@ -236,12 +163,10 @@ function moveHappyTimeImg(){
             picWrap.prevBtn = !picWrap.prevBtn;
         }
     }
-
 }
 
 
-
-
+//////////工具方法
 function mouseOverClearTimer(obj, timer, fn, times){     //鼠标移入清除定时器
     obj.onmouseover = function(){
         clearInterval(timer);
@@ -272,12 +197,13 @@ function doMove ( obj, attr, dir, target, endFn ){
     }, 20);
 }
 
-
 function cssDoMove(obj, dir, time, target, endFn){
-    obj.style[dir] = target + 'px';
-    obj.style.transition = time + 's linear';
-    setTimeout(function(){
-        obj.style.transition = '0s';
-        endFn && endFn();
-    }, 500)
+    setTimeout(function(){              //异步操作，防止多次定义样式覆盖问题
+        obj.style[dir] = target + 'px';
+        obj.style.transition = time + 's linear';
+        setTimeout(function(){
+            obj.style.transition = '0s';
+            endFn && endFn();
+        }, time*1000)
+    });
 }
