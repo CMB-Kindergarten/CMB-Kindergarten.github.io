9defc45fe00f002be84801809c0e972fb13f0c15
wzr1998 <253864593@qq.com>
2018-12-26 18:48:25 +0800

    index almost finish

--- js/public.js
+++ js/public.js
@@ -8,3 +8,25 @@ function addEvent(obj,type,fn){//obj目标对象,type事件类型,fn函数回调
     }
 
 }
+
+
+function mouseOverClearTimer(obj, timer, fn, times){     //鼠标移入清除定时器
+    obj.onmouseover = function(){
+        clearInterval(timer);
+    };
+    obj.onmouseout = function(){
+        clearInterval(timer);
+        timer = setInterval(fn, times);
+    };
+}
+
+function cssDoMove(obj, dir, time, target, endFn){
+    setTimeout(function(){              //异步操作，防止多次定义样式覆盖问题
+        obj.style[dir] = target + 'px';
+        obj.style.transition = time + 's linear';
+        setTimeout(function(){
+            obj.style.transition = '0s';
+            endFn && endFn();
+        }, time*1000)
+    });
+}
