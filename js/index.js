d7c9fba3a7bc1cb895edc9b2aebc253c88535a9c
wzr1998 <253864593@qq.com>
2018-12-26 11:38:32 +0800

    save content21

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ function window_onLoad(){
 
 
 
-function moveTopImg(){
+/*function moveTopImg(){
     let oPrompt = document.getElementById('prompt');
     let oPicWrap = oPrompt.getElementsByClassName('img')[0];
     let aImg = oPicWrap.getElementsByTagName('img');
@@ -61,7 +61,75 @@ function moveTopImg(){
             }
         }
     }
+}*/
+
+
+
+
+
+
+function moveTopImg(){
+    let oPrompt = document.getElementById('prompt');
+    let oPicWrap = oPrompt.getElementsByClassName('img')[0];
+    let aImg = oPicWrap.getElementsByTagName('img');
+    let aNum1 = oPrompt.getElementsByClassName('num')[0].getElementsByTagName('li');
+
+    let timer = setInterval(nextPic, 2500);
+    mouseOverClearTimer(oPicWrap, timer, nextPic, 2500);
+
+    oPicWrap.now = 0;               //记录显示区域中图片下标
+
+    nextPic();
+
+    function nextPic(){                         //自动轮播
+        for(let i=0, len=aNum1.length; i<len; i++){
+            aNum1[i].className = '';
+        }
+        if(oPicWrap.now === aImg.length-1){
+            aNum1[0].className = 'active';
+            cssDoMove(aImg[0], 'left', 0.5, 0);
+        }else{
+            aNum1[oPicWrap.now+1].className = 'active';
+            cssDoMove(aImg[oPicWrap.now+1], 'left', 0.5, 0);
+        }
+
+        cssDoMove(aImg[oPicWrap.now], 'left', 0.5, -270, function(){
+            cssDoMove(aImg[oPicWrap.now], 'left', 0, 270);
+            oPicWrap.now++;
+            oPicWrap.now %= aImg.length;
+        })
+
+
+    }
+
+    for(let i=0, len=aNum1.length; i<len; i++){
+        aNum1[i].onclick = function(){          //点击列表切换图片
+            for(let i=0, len=aNum1.length; i<len; i++){
+                aNum1[i].className = '';
+            }
+            aNum1[i].className = 'active';
+            /*if(i<oPicWrap.now){
+                aImg[i].style.left = -270 + 'px';
+                cssDoMove(aImg[i], 'left', 0.5, 0);
+                cssDoMove(aImg[oPicWrap.now], 'left', 0.5, 270, function(){
+                    oPicWrap.now = i;
+                })
+            }else if(i>oPicWrap.now){
+                cssDoMove(aImg[oPicWrap.now], 'left', 0.5, -270);
+                cssDoMove(aImg[i], 'left', 0.5, 0, function(){
+                    cssDoMove(aImg[oPicWrap.now], 'left', 0, -270);
+                    oPicWrap.now = i;
+                })
+            }*/
+        }
+    }
 }
+
+
+
+
+
+
 
 
 
@@ -91,12 +159,21 @@ function moveTopText(){
 function moveHappyTimeImg(){
     let picWrap = document.querySelector('#happyTime .pic');
     let aImg = document.querySelectorAll('#happyTime img');
+    let prev = document.querySelector('#happyTime .prev');
+    let next = document.querySelector('#happyTime .next');
     picWrap.now = 0;            //存放显示区中第一个图片下标
+    picWrap.prevBtn = true;
+    picWrap.nextBtn = true;
 
     let timer = setInterval(nextPic, 2000);
     mouseOverClearTimer(picWrap, timer, nextPic, 2000);
 
+    prev.onclick = prevPic;
+    next.onclick = nextPic;
+
     function nextPic(){
+        if(!picWrap.nextBtn) return;
+        picWrap.nextBtn = !picWrap.nextBtn;
         if(picWrap.now === aImg.length-3){
             doMove(aImg[picWrap.now], 'left', 10, -270);
             doMove(aImg[picWrap.now+1], 'left', 10, 0);
@@ -122,9 +199,12 @@ function moveHappyTimeImg(){
             aImg[picWrap.now].style.left = 810 + 'px';
             picWrap.now++;
             picWrap.now %= aImg.length;
+            picWrap.nextBtn = !picWrap.nextBtn;
         }
     }
     function prevPic(){
+        if(!picWrap.prevBtn) return;
+        picWrap.prevBtn = !picWrap.prevBtn;
         if(picWrap.now === 0){
             aImg[aImg.length-1].style.left = -270 + 'px';
             doMove(aImg[aImg.length-1], 'left', 10, 0);
@@ -152,7 +232,8 @@ function moveHappyTimeImg(){
         }
         function endFn(){
             picWrap.now--;
-            picWrap.now = picWrap.now === -1 ? aImg.length-1 : picWrap.now
+            picWrap.now = picWrap.now === -1 ? aImg.length-1 : picWrap.now;
+            picWrap.prevBtn = !picWrap.prevBtn;
         }
     }
 
@@ -189,4 +270,14 @@ function doMove ( obj, attr, dir, target, endFn ){
             endFn && endFn();
         }
     }, 20);
+}
+
+
+function cssDoMove(obj, dir, time, target, endFn){
+    obj.style[dir] = target + 'px';
+    obj.style.transition = time + 's linear';
+    setTimeout(function(){
+        obj.style.transition = '0s';
+        endFn && endFn();
+    }, 500)
 }
